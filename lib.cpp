b40fa44de33cb2ef6536f2c777c46f63843214ca
Francescodiodaa <91884945+Francescodiodaa@users.noreply.github.com>
2021-11-30 10:40:35 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
+//
+// Created by Francesco Dioda on 29/11/21.
+//
 
+#include "lib.h"
+#include "iostream"
+char funzione (char a){
+if (isalpha(a)){
+    return true;
+}
+else if (isdigit(a)){
+    return false;
+}}
