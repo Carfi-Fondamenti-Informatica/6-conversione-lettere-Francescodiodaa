af96f2579ad7c44600e58d9d6de60eaa5cfce0eb
Francescodiodaa <91884945+Francescodiodaa@users.noreply.github.com>
2021-11-30 10:40:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char a=0;
+    cin>>a;
+    if (funzione(a)==false){
+        cout<<"errore";
+    }
+    if (funzione(a)==true){
+        int ascii=(int)a;
+         if(ascii>=97){
+             ascii=(ascii-32);
+             cout<<char(ascii);
+        }
+        else{
+            cout<<char((ascii+32));
+        }
+
+    }
+
+    return 0;
 }
